0e09e6b9dea216326bce46eb89c30c120b669077
刘晨 <cliu3@xiaohongshu.com>
2018-06-05 19:38:29 +0800

    second commit

--- canvas.js
+++ canvas.js
@@ -1,18 +1,38 @@
 // canvas.js
-function innit(){
-            for(var i=0;i<tagEle.length;i++){
-                var a , b;
-                var k = -1+(2*(i+1)-1)/tagEle.length;
-                var a = Math.acos(k);
-                var b = a*Math.sqrt(tagEle.length*Math.PI);
-                // var a = Math.random()*2*Math.PI;
-                // var b = Math.random()*2*Math.PI;
-                var x = RADIUS * Math.sin(a) * Math.cos(b);
-                var y = RADIUS * Math.sin(a) * Math.sin(b); 
-                var z = RADIUS * Math.cos(a);
-                var t = new tag(tagEle[i] , x , y , z);
-                tagEle[i].style.color = "rgb("+parseInt(Math.random()*255)+","+parseInt(Math.random()*255)+","+parseInt(Math.random()*255)+")";
-                tags.push(t);
-                t.move();
-            }
-        }
+
+$(document).ready(function() {
+    var canvas = $('#spin')[0];
+    var winWidth = $('.canvas-container').width();
+    var winHeight = $('.canvas-container').width();
+
+    canvas.setAttribute('width', winWidth);
+    canvas.setAttribute('height', winHeight);
+
+    ctx = canvas.getContext('2d');
+    ctx.strokeStyle = "#fff";
+    // X-axis
+    line(winWidth/2, winHeight/2, winWidth, winHeight/2, ctx);
+    // Y-axis
+    line(winWidth/2, winHeight/2, winWidth/2, 0, ctx);
+    // Z-axis
+    line(winWidth/2, winHeight/2, 0, winHeight, ctx);
+});
+
+function line(sx, sy, ex, ey, ctx) {
+    ctx.moveTo(sx, sy);
+    ctx.lineTo(ex, ey);
+    ctx.stroke();
+}
+
+var Vector = function(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+
+    this._get2D = function() {
+        var scale = fallLength/(fallLength+this.z);
+        var x = centerX + this.x*scale;
+        var y = centerY + this.y*scale;
+        return {x:x , y:y};
+    }
+}
